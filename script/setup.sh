762c162b3be91c5905486e2ad952eafa90824ec3
Luke Boggs <lukeboggs7@gmail.com>
2023-12-28 15:19:48 -0600

    add to swtup script

--- script/setup.sh
+++ script/setup.sh
@@ -1,4 +1,6 @@
 sudo curl -o /usr/local/bin/cog -L https://github.com/replicate/cog/releases/latest/download/cog_`uname -s`_`uname -m`
 sudo chmod +x /usr/local/bin/cog
 
-cog build -t stable-dripfusion-2
+wget -c https://replicate.delivery/pbxt/K7ku1HCBJMUchwXERHHSMi4Vkm3W75Qox5Rt5nKG7kGYmgkf/trained_model.tar -O - | tar -xz
+
+sudo cog build -t stable-dripfusion-2
