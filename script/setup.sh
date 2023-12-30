ab08815d3962c76f8414f9292ab8c7353d055329
Luke Boggs <lukeboggs7@gmail.com>
2023-12-30 12:30:26 -0600

    add setup steps

--- script/setup.sh
+++ script/setup.sh
@@ -1,6 +1,7 @@
 sudo curl -o /usr/local/bin/cog -L https://github.com/replicate/cog/releases/latest/download/cog_`uname -s`_`uname -m`
 sudo chmod +x /usr/local/bin/cog
 
-wget -c https://replicate.delivery/pbxt/K7ku1HCBJMUchwXERHHSMi4Vkm3W75Qox5Rt5nKG7kGYmgkf/trained_model.tar -O - | tar -xz
-
-sudo cog build -t stable-dripfusion-2
+mkdir trained-model-luk
+mkdir trained-model-tok
+wget -c https://replicate.delivery/pbxt/K8l70F8kIPrIy6GDcoMok2k2C7EJSeWL3kQ4V52LKhsBqhe8/trained_model_luk.tar -O - | tar -C trained-model-luk -xz
+wget -c https://replicate.delivery/pbxt/K7ku1HCBJMUchwXERHHSMi4Vkm3W75Qox5Rt5nKG7kGYmgkf/trained_model.tar -O - | tar -C trained-model-tok -xz
