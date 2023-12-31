dac25edc8a5a5e115961828ba83b16afd4e80034
Luke Boggs <lukeboggs7@gmail.com>
2023-12-30 23:07:09 -0600

    do first thing first

--- predict.py
+++ predict.py
@@ -369,17 +369,20 @@ class Predictor(BasePredictor):
             sdxl_kwargs["cross_attention_kwargs"] = {"scale": lora_scale}
 
         output = pipe(**common_args, **sdxl_kwargs)
-        output2 = pipe2(**common_args, **sdxl_kwargs)
 
         output_paths = []
         for i, image in enumerate(output.images):
             output_path = f"/tmp/out-0{i}.png"
             image.save(output_path)
+            print("Saved image to: ", output_path)
             output_paths.append(Path(output_path))
 
+
+        output2 = pipe2(**common_args, **sdxl_kwargs)
         for i, image in enumerate(output2.images):
             output_path = f"/tmp/out-1{i}.png"
             image.save(output_path)
+            print("Saved image to: ", output_path)
             output_paths.append(Path(output_path))
         
         print("test")
