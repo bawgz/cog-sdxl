2d20db7cc79f26d267a51f8760da85690d73d005
Luke Boggs <lukeboggs7@gmail.com>
2024-01-02 22:05:32 -0600

    luk_man

--- script/train_luk.sh
+++ script/train_luk.sh
@@ -1,1 +1,1 @@
-sudo cog train -i input_images="https://replicate.delivery/pbxt/K5IafGgyGLIhAn3yqMSZe3H26Mrp44prmxorUpVhQnN7efQh/data.zip" -i token_string="LUK" -i use_face_detection_instead=True
+sudo cog train -i input_images="https://replicate.delivery/pbxt/K5IafGgyGLIhAn3yqMSZe3H26Mrp44prmxorUpVhQnN7efQh/data.zip" -i token_string="luk_man" -i caption_prefix="a photo of luk_man man, " -i use_face_detection_instead=True
